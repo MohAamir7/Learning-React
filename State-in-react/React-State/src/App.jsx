593265342f22788521dba5e23f641ccacaa4d9ad
Moh Aamir <mohaamir0706@gmail.com>
2026-08-21 18:41:32 +0530

    State with Nested Object

--- State-in-react/React-State/src/App.jsx
+++ State-in-react/React-State/src/App.jsx
@@ -5,6 +5,7 @@ import Counter from './State-over-Time'
 import Form from './Message-Timer'
 import MovingDot from './Object-in-State'
 import Form2 from './Spread-Copy'
+import NestedForm from './NestedObject-in-State'
 
 function App() {
 
@@ -15,6 +16,7 @@ function App() {
     {/* <Form></Form> */}
     {/* <MovingDot></MovingDot> */}
     <Form2></Form2>
+    <NestedForm></NestedForm>
 
       
     </>
