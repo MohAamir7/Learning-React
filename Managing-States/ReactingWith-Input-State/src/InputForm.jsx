b19d9c481888902ef0eb66ca10a038af46837428
Moh Aamir <mohaamir0706@gmail.com>
2026-08-22 18:38:51 +0530

    making a little quiz game pretend like network call to understand the state managing

--- Managing-States/ReactingWith-Input-State/src/InputForm.jsx
+++ Managing-States/ReactingWith-Input-State/src/InputForm.jsx
@@ -1,6 +1,29 @@
-function Form({ status = "empty" }) {
+import { useState } from "react";
+
+function Form() {
+  const [answer,SetAnswer] = useState('');
+  const [isError,setIsError] = useState(null);
+  const [status,setStatus] = useState('typing');
   if (status === "success") {
     return <h1>That's right Answer</h1>;
+  }
+
+  async function handleSubmit(e){
+    e.preventDefault();
+    setStatus('submitting');
+    try{
+       await submitForm(answer);
+       setStatus('success');
+    }catch(err){
+      setStatus('typing');
+      setIsError(err);
+
+    }
+
+  }
+
+  function handleTextareaChange(e){
+    SetAnswer(e.target.value);
   }
 
   return (
@@ -9,18 +32,34 @@ function Form({ status = "empty" }) {
       <p>
         In which city is there a billboard that turns air into drinkable water?
       </p>
-      <form>
-        <textarea placeholder="Enter your answer here...." disabled={status==='submitting'}></textarea>
+      <form onSubmit={handleSubmit}>
+        <textarea value={answer}
+        onChange={handleTextareaChange}
+        placeholder="Enter your answer here...." disabled={status==='submitting'}></textarea>
         <button disabled={
-            status === 'empty' || 'submitting'
+           answer.length === 0 || status==='submitting'
         }>Submit</button>
 
-        {status === 'error' && 
-            <p className="Error">Good guess but wrong answer.Try again...</p>
+        {isError !== null && 
+            <p className="Error">{isError.message}</p>
         }
       </form>
     </>
   );
+}
+
+async function submitForm(answer) {
+  return new Promise((res,rej)=>{
+    setTimeout(()=>{
+      let shoulderr = answer.toLowerCase() !== 'lima'
+      if(shoulderr){
+        rej(new Error('Good guess But wrong answer. Try again..'));
+      }else{
+        res();
+      }
+    },3000);
+  })
+  
 }
 
 export default Form;
